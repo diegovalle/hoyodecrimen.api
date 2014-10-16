dfe674d90e8aaf085c5299036fba8da99d26f99b
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-16 14:41:29 -0500

    cuadrantes api

--- wsgi/api.py
+++ wsgi/api.py
@@ -43,10 +43,17 @@ class Cuadrantes(db.Model):
 def index():
     return "Hello from API"
 
+@app.route('/v1/cuadrantes/'
+          '<string:cuadrante_id>',
+          methods=['GET'])
+    if request.method == 'GET':
+        cuadrante_id = cuadrante.lower()
+        results = db.session.execute("select cuadrante, sector, crime, date, count from cuadrantes order by crime, date, cuadrante, sector where cuadrante = ?", [cuadrante_id])
+
 @app.route('/v1/top5/cuadrantes')
 def top5cuadrantes():
     results = db.session.execute("""with crimes as
-(select sum(count) as count,sector,cuadrante,max(population)as population, crime from cuadrantes  where date >= '2013-08-01' and date <= '2014-07-01' group by cuadrante, sector, crime)
+(select sum(count) as count,sector,cuadrante,max(population)as population, crime from cuadrantes where date >= '2013-08-01' and date <= '2014-07-01' group by cuadrante, sector, crime)
 SELECT * from (SELECT count,crime,sector,cuadrante,rank() over (partition by crime order by count desc) as rank,population from crimes group by count,crime,sector,cuadrante,population) as temp2 where rank <= (SELECT rank from (SELECT count,cuadrante,rank() over (partition by crime order by count desc) as rank, row_number() OVER (ORDER BY count desc) AS rownum from crimes) as rank10 where rownum = 10) order by crime, rank,sector, cuadrante""")
     json_results = []
     for result in results:
