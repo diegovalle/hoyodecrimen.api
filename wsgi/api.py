f3f8195d7920722fad29571bf3cef533e3075bce
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-16 15:12:27 -0500

    cuadrantes api

--- wsgi/api.py
+++ wsgi/api.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import Flask, jsonify, request
-#from flask_sqlalchemy import SQLAlchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
  
 app = Flask(__name__)
  
