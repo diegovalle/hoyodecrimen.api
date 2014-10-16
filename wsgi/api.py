3dc0ca30e9fd84ecec786478c05003cfdb2c8b80
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-16 18:09:52 -0500

    new api endpoints

--- wsgi/api.py
+++ wsgi/api.py
@@ -142,7 +142,7 @@ def listsectores():
 def top5cuadrantes():
     results = db.session.execute("""with crimes as
 (select sum(count) as count,sector,cuadrante,max(population)as population, crime from cuadrantes where date >= '2013-08-01' and date <= '2014-07-01' group by cuadrante, sector, crime)
-SELECT * from (SELECT count,crime,sector,cuadrante,rank() over (partition by crime order by count desc) as rank,population from crimes group by count,crime,sector,cuadrante,population) as temp2 where rank <= (SELECT rank from (SELECT count,cuadrante,rank() over (partition by crime order by count desc) as rank, row_number() OVER (ORDER BY count desc) AS rownum from crimes) as rank10 where rownum = 10) order by crime, rank,sector, cuadrante""")
+SELECT * from (SELECT count,crime,sector,cuadrante,rank() over (partition by crime order by count desc) as rank,population from crimes group by count,crime,sector,cuadrante,population) as temp2 where rank <= 5""")
     json_results = []
     for result in results:
             d = {'count': result.count,
@@ -153,6 +153,40 @@ SELECT * from (SELECT count,crime,sector,cuadrante,rank() over (partition by cri
                  'population': result.population}
             json_results.append(d)
     return jsonify(items = json_results)
+
+@app.route('/v1/top5/sectores')
+def top5sectores():
+    results = db.session.execute("""with crimes as
+(select (sum(count) / (sum(population) /12 )* 100000) as rate,sum(count) as count,sector,sum(population)/12 as population, crime from cuadrantes  where date >= '2013-08-01' and date <= '2014-07-01' group by sector, crime)
+SELECT * from (SELECT count,rate,crime,sector,rank() over (partition by crime order by rate desc) as rank,population from crimes group by count,crime,sector,population, rate) as temp2 where rank <= 5""")
+    json_results = []
+    for result in results:
+            d = {'count': result.count,
+                 'crime': result.crime,
+                 'sector': result.sector,
+                 'cuadrante': result.cuadrante,
+                 'rank': result.rank,
+                 'population': result.population}
+            json_results.append(d)
+    return jsonify(items = json_results)
+
+@app.route('/v1/top5/changecuadrantes')
+def top5sectores():
+    results = db.session.execute("""with difference as
+(select crime, cuadrante, sector, max(population) as population, sum(case when date = '2014-07-01' or date = '2014-06-01' or date='2014-05-01' THEN count ELSE 0 END) as y2014, sum(case when date = '2013-07-01' or date = '2013-06-01' or date='2013-05-01' THEN count ELSE 0 END) as y2013, sum(case when date = '2014-07-01' or date = '2014-06-01' or date='2014-05-01' THEN count ELSE 0 END) - sum(case when date = '2013-07-01' or date = '2013-06-01' or date='2013-05-01' THEN count ELSE 0 END) as diff  from cuadrantes group by cuadrante, sector, crime order by diff desc)
+SELECT * from (SELECT rank() over (partition by crime order by diff desc) as rank,crime,cuadrante,sector,population, y2013, y2014,diff from difference group by diff,crime,sector,cuadrante, population, y2013, y2014) as temp where rank <= 5""")
+    json_results = []
+    for result in results:
+            d = {'count': result.count,
+                 'crime': result.crime,
+                 'sector': result.sector,
+                 'cuadrante': result.cuadrante,
+                 'rank': result.rank,
+                 'population': result.population}
+            json_results.append(d)
+    return jsonify(items = json_results)
+
+
  
 if __name__ == '__main__':
     app.run()
