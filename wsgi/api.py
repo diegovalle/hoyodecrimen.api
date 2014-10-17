e312b398b22b049ebfc6e82216ec5384cb48794d
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-17 16:42:23 -0500

    new api endpoints

--- wsgi/api.py
+++ wsgi/api.py
@@ -112,7 +112,7 @@ def index():
 def df_all_crime():
     if request.method == 'GET':
         results = Cuadrantes.query. \
-            with_entities(Cuadrantes.crime,
+            with_entities(func.lower(Cuadrantes.crime),
                           Cuadrantes.date,
                           func.sum(Cuadrantes.count).label('count'),
                           func.sum(Cuadrantes.population).label('population')). \
@@ -130,7 +130,7 @@ def df_all(crime):
     if request.method == 'GET':
         results = Cuadrantes.query. \
             filter(Cuadrantes.crime == crime). \
-            with_entities(Cuadrantes.crime,
+            with_entities(func.lower(Cuadrantes.crime),
                           Cuadrantes.date,
                           func.sum(Cuadrantes.count).label('count'),
                           func.sum(Cuadrantes.population).label('population')). \
@@ -148,9 +148,9 @@ def cuadrantes(crime, cuadrante):
         results = Cuadrantes.query. \
             filter(Cuadrantes.cuadrante == cuadrante,
                    Cuadrantes.crime == crime). \
-            with_entities(Cuadrantes.cuadrante,
-                          Cuadrantes.sector,
-                          Cuadrantes.crime,
+            with_entities(func.lower(Cuadrantes.cuadrante),
+                          func.lower(Cuadrantes.sector),
+                          func.lower(Cuadrantes.crime),
                           Cuadrantes.date,
                           Cuadrantes.count,
                           Cuadrantes.population) \
@@ -168,8 +168,8 @@ def sectors(crime, sector):
         results = Cuadrantes.query. \
             filter(Cuadrantes.sector == sector,
                    Cuadrantes.crime == crime). \
-            with_entities(Cuadrantes.sector,
-                          Cuadrantes.crime,
+            with_entities(func.lower(Cuadrantes.sector),
+                          func.lower(Cuadrantes.crime),
                           Cuadrantes.date,
                           func.sum(Cuadrantes.count).label('count'),
                           func.sum(Cuadrantes.population).label('population')). \
@@ -181,7 +181,7 @@ def sectors(crime, sector):
 @app.route('/v1/list/crimes')
 def listcrimes():
     results = Cuadrantes.query. \
-              with_entities(Cuadrantes.crime).\
+              with_entities(func.lower(Cuadrantes.crime)).\
               distinct().\
               all()
     json_results = []
@@ -193,7 +193,7 @@ def listcrimes():
 @app.route('/v1/list/cuadrantes')
 def listcuadrantes():
     results = Cuadrantes.query. \
-              with_entities(Cuadrantes.sector, Cuadrantes.cuadrante).\
+              with_entities(func.lower(Cuadrantes.sector), func.lower(Cuadrantes.cuadrante)).\
               distinct().\
               all()
     return results_to_json(results)
@@ -201,7 +201,7 @@ def listcuadrantes():
 @app.route('/v1/list/sectores')
 def listsectores():
     results = Cuadrantes.query. \
-              with_entities(Cuadrantes.sector).\
+              with_entities(func.lower(Cuadrantes.sector)).\
               distinct().\
               all()
     return results_to_json(results)
@@ -219,7 +219,19 @@ def top5cuadrantes():
 (select sum(count) as count,sector,cuadrante,max(population)as population, crime from cuadrantes where date >= '{0}' and date <= '{1}' group by cuadrante, sector, crime)
 SELECT * from (SELECT count,crime,sector,cuadrante,rank() over (partition by crime order by count desc) as rank,population from crimes group by count,crime,sector,cuadrante,population) as temp2 where rank <= 5 order by crime, count, cuadrante, sector desc""".format(start_date, max_date)
     results = db.session.execute(sql_query)
-    return results_to_json(results)
+    json_results = []
+    keys = results[0].keys()
+    for result in results:
+        d = {}
+        for i, key in enumerate(keys):
+            if key == "date":
+                d["year"] = int(result[i][0:4])
+                d["month"] = int(result[i][5:7])
+            else:
+                d[key] = result[i]
+        json_results.append(d)
+
+    return jsonify(rows=json_results)  #Response(json.dumps(json_results),  mimetype='application/json')#
 
 @app.route('/v1/top5/sectorrate')
 def top5sectores():
@@ -232,7 +244,19 @@ def top5sectores():
 (select (sum(count) / (sum(population::float) /12 )* 100000) as rate,sum(count) as count,sector,sum(population)/12 as population, crime from cuadrantes  where date >= '{0}' and date <= '{1}' group by sector, crime)
 SELECT * from (SELECT count,rate,crime,sector,rank() over (partition by crime order by rate desc) as rank,population from crimes group by count,crime,sector,population, rate) as temp2 where rank <= 5""".format(start_date, max_date)
     results = db.session.execute(sql_query)
-    return results_to_json(results)
+    json_results = []
+    keys = results[0].keys()
+    for result in results:
+        d = {}
+        for i, key in enumerate(keys):
+            if key == "date":
+                d["year"] = int(result[i][0:4])
+                d["month"] = int(result[i][5:7])
+            else:
+                d[key] = result[i]
+        json_results.append(d)
+
+    return jsonify(rows=json_results)  #Response(json.dumps(json_results),  mimetype='application/json')#
 
 @app.route('/v1/top5/changecuadrantes')
 def top5changecuadrantes():
@@ -254,7 +278,19 @@ def top5changecuadrantes():
 (select crime, cuadrante, sector, max(population) as population, sum(case when date = '2014-07-01' or date = '2014-06-01' or date='2014-05-01' THEN count ELSE 0 END) 
 as end_period, sum(case when date = '2013-07-01' or date = '2013-06-01' or date='2013-05-01' THEN count ELSE 0 END) as start_period, sum(case when date = '2014-07-01' or date = '2014-06-01' or date='2014-05-01' THEN count ELSE 0 END) - sum(case when date = '2013-07-01' or date = '2013-06-01' or date='2013-05-01' THEN count ELSE 0 END) as diff  from cuadrantes group by cuadrante, sector, crime order by diff desc)
 SELECT * from (SELECT rank() over (partition by crime order by diff desc) as rank,crime,cuadrante,sector,population, start_period, end_period,diff from difference group by diff,crime,sector,cuadrante, population, start_period, end_period) as temp where rank <= 5  order by crime, rank, cuadrante, sector asc""")
-    return results_to_json(results)
+    json_results = []
+    keys = results[0].keys()
+    for result in results:
+        d = {}
+        for i, key in enumerate(keys):
+            if key == "date":
+                d["year"] = int(result[i][0:4])
+                d["month"] = int(result[i][5:7])
+            else:
+                d[key] = result[i]
+        json_results.append(d)
+
+    return jsonify(rows=json_results)  #Response(json.dumps(json_results),  mimetype='application/json')#
 
 
  
