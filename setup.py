a727738013bc87f2e7214a17dcd4b9f39257dec8
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-16 20:30:30 -0500

    new api endpoints

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Diego Valle-Jones',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 'SQLAlchemy==0.9.3'],
+      install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 'SQLAlchemy==0.9.3', 'redis==2.9.1','Flask-Cache==0.12'],
      )
