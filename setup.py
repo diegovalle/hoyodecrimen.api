8f648faaa9feb760c1ea63db25e15e657551a599
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-18 17:15:17 -0500

    geoalchemy

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(name='YourAppName',
       author='Diego Valle-Jones',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 'SQLAlchemy==0.9.3', 'redis==2.9.1','Flask-Cache==0.12'],
+      install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 
+                        'SQLAlchemy==0.9.3', 'redis==2.9.1','Flask-Cache==0.12',
+                        'GeoAlchemy2'==0.2.4],
      )
