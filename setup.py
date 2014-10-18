de583017c5b0697c28c0032a1f39700fadd38d42
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-18 17:16:02 -0500

    description

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='YourAppName',
       version='1.0',
-      description='OpenShift App',
+      description='Hoyo de Crimen',
       author='Diego Valle-Jones',
-      author_email='example@example.com',
+      author_email='diegovalle@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 
                         'SQLAlchemy==0.9.3', 'redis==2.9.1','Flask-Cache==0.12',
