67385165d15f0b6e90326e335a2d301872a90e85
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-22 19:06:59 -0500

    define the blueprint dir

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.append(os.path.abspath('ext'))
-sys.path.append(os.path.abspath('../wsgi'))
+sys.path.append(os.path.abspath('../wsgi/'))
 extensions = ['sphinxcontrib.httpdomain', 'sphinxcontrib.autohttp.flask']
 
 # Add any paths that contain templates here, relative to this directory.
