dcc55b1dee100aeb7527fad2cce5397b84a2d988
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-28 09:35:48 -0600

    expire redis keys in 30 days

--- wsgi/api/api.py
+++ wsgi/api/api.py
@@ -20,7 +20,7 @@ else:
     cache = Cache(config={
         'CACHE_TYPE': 'redis',
         #'CACHE_REDIS_URL': 'redis://:' +os.environ['REDIS_PASSWORD'] +'@' + os.environ['OPENSHIFT_REDIS_HOST'] + ':'+ os.environ['OPENSHIFT_REDIS_PORT'],
-        #'CACHE_DEFAULT_TIMEOUT': 922337203685477580,
+        'CACHE_DEFAULT_TIMEOUT': 2592000 ,
         'CACHE_REDIS_PASSWORD': os.environ['REDIS_PASSWORD'],
         'CACHE_REDIS_HOST': os.environ['OPENSHIFT_REDIS_HOST'],
         'CACHE_REDIS_PORT': os.environ['OPENSHIFT_REDIS_PORT'],
