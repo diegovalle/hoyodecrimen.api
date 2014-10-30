b1520a223f285be05c9eeaf80b74856668988391
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-29 22:06:50 -0600

    new paths

--- wsgi/flaskr_tests.py
+++ wsgi/flaskr_tests.py
@@ -17,13 +17,13 @@ class FlaskTestCase(unittest.TestCase):
 	# Check the API endpoint
     def test_api_v1_top_counts_change_cuadrantes(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/top/counts/change/cuadrantes/homicidio%20doloso', content_type='application/json')
+        response = tester.get('/api/v1/cuadrantes/crimes/homicidio%20doloso/top/counts/change', content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(json.loads(response.data), {"rows": []})
 
     # Check that the API correctly deals with date parameters
     def test_error_api_v1_top_counts_change_cuadrantes_dates(self):
-        base_url = '/api/v1/top/counts/change/cuadrantes/homicidio%20doloso'
+        base_url = '/api/v1/cuadrantes/crimes/homicidio%20doloso/top/counts/change'
         tester = app.test_client(self)
         response = tester.get(base_url + '?start_period1=2013-01&end_period1=2013-12&start_period2=2014-01&end_period2=2014-06', content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -51,7 +51,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Check that the API correctly deals with date parameters
     def test_top_rates_sectores(self):
-        base_url = '/api/v1/top/rates/sectores/homicidio%20doloso'
+        base_url = '/api/v1/sectores/crimes/homicidio%20doloso/top/rates'
         tester = app.test_client(self)
         response = tester.get(base_url + '?start_date=2013-08&end_date=2014-07', content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -68,7 +68,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Check that the API correctly deals with date parameters
     def test_top_counts_cuadrantes(self):
-        base_url = '/api/v1/top/counts/cuadrantes/homicidio%20doloso'
+        base_url = '/api/v1/cuadrantes/crimes/homicidio%20doloso/top/counts'
         tester = app.test_client(self)
         response = tester.get(base_url + '?start_date=2013-08&end_date=2014-07', content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -86,27 +86,27 @@ class FlaskTestCase(unittest.TestCase):
     # Check the API endpoint
     def test_api_v1_enumerate_sectores(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/enumerate/sectores', content_type='application/json')
+        response = tester.get('/api/v1/sectores/enumerate', content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(json.loads(response.data), {"rows": []})
 
     # Check the API endpoint
     def test_api_v1_enumerate_cuadrantes(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/enumerate/cuadrantes', content_type='application/json')
+        response = tester.get('/api/v1/cuadrantes/enumerate', content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(json.loads(response.data), {"rows": []})
 
     # Check the API endpoint
     def test_api_v1_enumerate_crimes(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/enumerate/crimes', content_type='application/json')
+        response = tester.get('/api/v1/crimes/enumerate', content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(json.loads(response.data), {"rows": []})
 
     # Check the API endpoint
     def test_api_v1_list_change_cuadrantes_all(self):
-        base_url = '/api/v1/list/change/cuadrantes/all'
+        base_url = '/api/v1/cuadrantes/crimes/all/period/change'
         tester = app.test_client(self)
         response = tester.get(base_url + '?start_period1=2013-01&end_period1=2013-12&start_period2=2014-01&end_period2=2014-06', content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -135,7 +135,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Check the API endpoint
     def test_api_v1_list_sectores_all(self):
-        base_url = '/api/v1/list/sectores/all'
+        base_url = '/api/v1/sectores/crimes/all/period'
         tester = app.test_client(self)
         response = tester.get(base_url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -156,7 +156,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Check the API endpoint
     def test_api_v1_list_cuadrantes_all(self):
-        base_url = '/api/v1/list/cuadrantes/all'
+        base_url = '/api/v1/cuadrantes/crimes/all/period'
         tester = app.test_client(self)
         response = tester.get(base_url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -177,7 +177,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Check the API endpoint
     def test_api_v1_series_sectores_a(self):
-        base_url = '/api/v1/series/sector/angel%20-%20zona%20rosa/violacion'
+        base_url = '/api/v1/sector/angel%20-%20zona%20rosa/crimes/violacion/series'
         tester = app.test_client(self)
         response = tester.get(base_url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -198,7 +198,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Check the API endpoint
     def test_api_v1_series_cuadrantes(self):
-        base_url = '/api/v1/series/cuadrante/c-1.1.1/violacion'
+        base_url = '/api/v1/cuadrante/c-1.1.1/crimes/violacion/series'
         tester = app.test_client(self)
         response = tester.get(base_url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -211,8 +211,8 @@ class FlaskTestCase(unittest.TestCase):
         self.assertNotEqual(json.loads(response.data), {"rows": []})
 
     # Check the API endpoint
-    def test_api_v1_top_counts_change_cuadrantes(self):
-        base_url = '/api/v1/series/df/violacion'
+    def test_df_series(self):
+        base_url = '/api/v1/df/crimes/violacion/series'
         tester = app.test_client(self)
         response = tester.get(base_url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -229,7 +229,7 @@ class FlaskTestCase(unittest.TestCase):
     # Check the API endpoint
     def test_api_v1_pip_extras_(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/pip/extras/-99.13333/19.43', content_type='application/json')
+        response = tester.get('/api/v1/pip/-99.13333/19.43/extras', content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(json.loads(response.data), {"rows": []})
 
