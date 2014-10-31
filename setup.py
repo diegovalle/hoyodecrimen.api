588bdc9ed62f38770a574b95bc4a694290253a4c
Diego Valle-Jones <diegovalle@gmail.com>
2014-10-30 20:45:06 -0600

    assets, babel and compress

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='YourAppName',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 
                         'SQLAlchemy==0.9.3', 'redis==2.9.1','Flask-Cache==0.12',
-                        'GeoAlchemy2==0.2.4'],
+                        'GeoAlchemy2==0.2.4', 'Babel==1.3',
+                        'Flask-Assets==0.10', 'Flask-Compress==1.0.2'],
      )
