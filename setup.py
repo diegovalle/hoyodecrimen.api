aa0b04e8b36c331f88d7f4d1cd9adaf7b1e16780
Diego Valle-Jones <diegovalle@gmail.com>
2014-11-01 20:31:55 -0600

    add flask-babel

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='YourAppName',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask==0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy==1.0', 
                         'SQLAlchemy==0.9.3', 'redis==2.9.1','Flask-Cache==0.12',
-                        'GeoAlchemy2==0.2.4', 'Babel==1.3',
+                        'GeoAlchemy2==0.2.4', 'Babel==1.3', 'Flask-Babel==0.9',
                         'Flask-Assets==0.10', 'Flask-Compress==1.0.2',
                         'jsmin==2.0.11', 'cssmin==0.2.0'],
      )
